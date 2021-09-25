5fa78d60d639d7cfbf6b793932997f8eddce6422
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-09-25 09:32:20 +0530

    Added:DAY_2 Program Added

---
+++ src/day2/RunTimeError.java
@@ -0,0 +1,8 @@
+package day2;
+
+public class RunTimeError {
+    public static void main(String[] args) {
+        int a = 10, b = 0;
+        System.out.println("Enter a result" + a / b);
+    }
+}
